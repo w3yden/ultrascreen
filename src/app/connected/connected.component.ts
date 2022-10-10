cffd6ed1137d9cfc1d7e1b09d8213acbfb3ca51c
w3yden <w3yden@protonmail.com>
2022-10-10 13:27:42 +0200

    Add copy to clipboard functionality & mimic more connected vue component

--- src/app/connected/connected.component.ts
+++ src/app/connected/connected.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PeerConnection, PeerJsService } from '../core/services/peer-js.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-connected',
@@ -8,29 +9,44 @@ import { PeerConnection, PeerJsService } from '../core/services/peer-js.service'
   styleUrls: ['./connected.component.scss']
 })
 export class ConnectedComponent implements OnInit {
+  nickname: string;
+  peerId: string;
+  connectionList: Array<string>;
+  newPeerId = '';
 
-  constructor(private router: Router, private peerJsService: PeerJsService) {   }
-
-  nickname: string
-  peerId: string
+  constructor(private router: Router, private peerJsService: PeerJsService, private toastrService: ToastrService) {   }
 
   ngOnInit(): void {
+    this.connectionList = new Array<string>();
+
     this.peerJsService.connection.subscribe((connection: PeerConnection | null) => {
-      console.log("Update: ", connection)
+      console.log('Update: ', connection);
       if(connection === null) {
         setTimeout(() => {
-          this.router.navigate(['home'])
-        }, 200)
-        return
+          this.router.navigate(['home']);
+        }, 200);
+        return;
       }
-      this.nickname = connection?.nickname
-      this.peerId = connection?.id
+      this.nickname = connection?.nickname;
+      this.peerId = connection?.id;
     });
     console.log('DetailComponent INIT');
    }
 
+   copyMyId() {
+    navigator.clipboard.writeText(this.peerId);
+    this.toastrService.info('Copied!', null, {timeOut: 1000});
+   }
+
    disconnect() {
-    this.peerJsService.disconnect()
+    this.peerJsService.disconnect();
+   }
+
+   addPeer() {
+   }
+
+   openCredits() {
+
    }
 
 }
